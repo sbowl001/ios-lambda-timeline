e6c82a9414e6b0eba9f97a4800026d6349061c29
Stephanie <sbowl001@fiu.edu>
2019-09-23 11:25:48 +0800

    comment refractor

--- LambdaTimeline/Models/Comment.swift
+++ LambdaTimeline/Models/Comment.swift
@@ -12,32 +12,39 @@ import FirebaseAuth
 class Comment: FirebaseConvertible, Equatable {
     
     static private let textKey = "text"
+    static private let audioURLKey = "audioURL"
     static private let author = "author"
     static private let timestampKey = "timestamp"
     
-    let text: String
+    let text: String?
+    let audioURL: URL?
     let author: Author
     let timestamp: Date
     
-    init(text: String, author: Author, timestamp: Date = Date()) {
+    init(text: String?, audioURL: URL? = nil, author: Author, timestamp: Date = Date()) {
         self.text = text
+        self.audioURL = audioURL
         self.author = author
         self.timestamp = timestamp
+        
     }
     
     init?(dictionary: [String : Any]) {
-        guard let text = dictionary[Comment.textKey] as? String,
+        guard let text = dictionary[Comment.textKey] as? String?,
+            let audioURL = dictionary[Comment.audioURLKey] as? URL?,
             let authorDictionary = dictionary[Comment.author] as? [String: Any],
             let author = Author(dictionary: authorDictionary),
             let timestampTimeInterval = dictionary[Comment.timestampKey] as? TimeInterval else { return nil }
         
         self.text = text
+        self.audioURL = audioURL
         self.author = author
         self.timestamp = Date(timeIntervalSince1970: timestampTimeInterval)
     }
     
     var dictionaryRepresentation: [String: Any] {
-        return [Comment.textKey: text,
+        return [Comment.textKey: text ?? "",
+                Comment.audioURLKey: audioURL,
                 Comment.author: author.dictionaryRepresentation,
                 Comment.timestampKey: timestamp.timeIntervalSince1970]
     }
---
+++ LambdaTimeline/View Controllers/AudioCellTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  AudioCellTableViewCell.swift
+//  LambdaTimeline
+//
+//  Created by Stephanie Bowles on 9/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AudioCellTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+    
+    @IBAction func playButtonTapped(_ sender: Any) {
+    }
+}
