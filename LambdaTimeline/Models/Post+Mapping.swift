f946451270533ed8603c17744231db8701d028d1
Stephanie <sbowl001@fiu.edu>
2019-09-30 10:13:13 +0800

    changes

---
+++ LambdaTimeline/Models/Post+Mapping.swift
@@ -0,0 +1,12 @@
+//
+//  Post+Mapping.swift
+//  LambdaTimeline
+//
+//  Created by Stephanie Bowles on 9/27/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+
